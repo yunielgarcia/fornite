96f3e2d64438ecb9473f36a2de35789a2135b4da
Yuniel Garcia <garciayuni84@gmail.com>
2018-06-13 17:59:34 -0400

    working on tabs

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -11,77 +11,92 @@
                 {
                     name: 'Name?',
                     src: 'assets/media/images/01.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
 
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/02.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/03.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/04.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/05.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/06.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/07.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/08.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/09.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/10.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/11.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/12.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/13.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/14.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 },
                 {
                     name: 'Name?',
                     src: 'assets/media/images/15.jpg',
+                    description: 'These virtuous knights descend upon the earthen plane to undo the evil plaguing it.',
                     selected: false
                 }
             ]
@@ -109,6 +124,22 @@
                 });
             };
 
+            vm.openRedeem = function () {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'redeemModal.html',
+                    controller: 'RedeemModalInstanceCtrl',
+                    controllerAs: 'vm',
+                    size: 'md',
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+                    // $ctrl.selected = selectedItem;
+                }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                });
+            };
+
 
         })
         .controller('ModalInstanceCtrl', function ($uibModalInstance, item_fig) {
@@ -117,6 +148,18 @@
 
             vm.ok = function () {
                 // $uibModalInstance.close(vm.selected.item);
+                $uibModalInstance.close();
+            };
+
+            vm.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+        })
+        .controller('RedeemModalInstanceCtrl', function ($uibModalInstance) {
+            var vm = this;
+
+            vm.ok = function () {
+                $uibModalInstance.close();
             };
 
             vm.cancel = function () {
