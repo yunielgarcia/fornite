567d8565c7035d6aa496f034704588449f154e3b
Yuniel Garcia <garciayuni84@gmail.com>
2018-06-13 09:59:12 -0400

    passing selected item to modal

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,125 +1,128 @@
-angular.module('app', ['ui.bootstrap'])
-    .controller('appCtrl', function ($uibModal, $log) {
+(function(){
 
-        var vm = this;
+    "use strict";
 
-        vm.items = [
-            {
-                name: 'Name?',
-                src: 'assets/media/images/01.jpg',
-                selected: false
+    angular.module('app', ['ui.bootstrap'])
+        .controller('appCtrl', function ($uibModal, $log) {
 
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/02.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/03.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/04.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/05.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/06.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/07.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/08.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/09.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/10.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/11.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/12.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/13.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/14.jpg',
-                selected: false
-            },
-            {
-                name: 'Name?',
-                src: 'assets/media/images/15.jpg',
-                selected: false
-            }
-        ]
+            var vm = this;
 
-        vm.open = function (size) {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'myModalContent.html',
-                controller: 'ModalInstanceCtrl',
-                controllerAs: 'vm',
-                size: size,
-                resolve: {
-                    items: function () {
-                        // return $ctrl.items;
-                    }
+            vm.items = [
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/01.jpg',
+                    selected: false
+
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/02.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/03.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/04.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/05.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/06.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/07.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/08.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/09.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/10.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/11.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/12.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/13.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/14.jpg',
+                    selected: false
+                },
+                {
+                    name: 'Name?',
+                    src: 'assets/media/images/15.jpg',
+                    selected: false
                 }
-            });
+            ]
 
-            modalInstance.result.then(function (selectedItem) {
-                // $ctrl.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        };
+            vm.open = function (item_fig) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'myModalContent.html',
+                    controller: 'ModalInstanceCtrl',
+                    controllerAs: 'vm',
+                    size: 'lg',
+                    resolve: {
+                        item_fig: function () {
+                            return item_fig;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+                    // $ctrl.selected = selectedItem;
+                }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                });
+            };
 
 
-    })
-    .controller('ModalInstanceCtrl', function ($uibModalInstance) {
-        var vm = this;
-        // vm.items = items;
-        // vm.selected = {
-        //     item: vm.items[0]
-        // };
+        })
+        .controller('ModalInstanceCtrl', function ($uibModalInstance, item_fig) {
+            var vm = this;
+            vm.fig = item_fig;
 
-        vm.ok = function () {
-            // $uibModalInstance.close(vm.selected.item);
-        };
+            vm.ok = function () {
+                // $uibModalInstance.close(vm.selected.item);
+            };
 
-        vm.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
-    })
-;
+            vm.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+        })
+    ;
+})();
+
